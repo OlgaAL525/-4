14ed9eeab2d04bbce5df63c2952d1a39c165c705
OlgaAL525 <mouse5525@gmail.com>
2024-10-04 23:42:49 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from true_math import divide as math
+
+from fake_math import divide as school
+
+
+result1 = school(69,3)
+result2 = school(3,0)
+result3 = math(49,7)
+result4 = math(15,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
